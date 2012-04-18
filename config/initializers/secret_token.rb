78c3b572f8f2f7b018b2907d202e02fcd9927a09
Don Pottinger <don.pottinger@gmail.com>
2012-04-18 11:23:28 -0400

    Generated Rails app with initial set of gems [#28231299]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Widgetfactory::Application.config.secret_token = 'c212b73f0c10681b82bc1f440c7bd9b1623fcb4c9b626470db1b45b79cd55ef2afd669cdb8f78cb242e25f5e893f1ca58a1f45c1726353d23d2d294caf29621d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Widgetfactory::Application.config.session_store :cookie_store, key: '_widgetfactory_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Widgetfactory::Application.config.session_store :active_record_store

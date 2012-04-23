3bc3c0b07624564a7a873f255f5387c1e6dfe2bf
Don Pottinger <don.pottinger@gmail.com>
2012-04-23 11:34:47 -0400

    [Finish #28460595] created helper method to convert date to readable format.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  include ApplicationHelper
+
   protect_from_forgery
 
   http_basic_authenticate_with :name => "admin", :password => "allthewidgets" if Rails.env.production?
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def make_readable(date)
+    date.in_time_zone("Eastern Time (US & Canada)").to_formatted_s(:long_ordinal)
+  end
 end
